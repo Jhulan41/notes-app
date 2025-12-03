5debd37f153c12ec4df4d00388ddcf0699ecf9f0
Jhulan41 <jhulan41@gmail.com>
2025-12-04 00:39:24 +0530

    Added edit functionality to the notes and also updated .gitignore file

--- notes-app-frontend/src/app/pages/update-note/update-note.ts
+++ notes-app-frontend/src/app/pages/update-note/update-note.ts
@@ -1,11 +1,55 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { ActivatedRoute, Router } from '@angular/router';
+import { NoteService } from '../../services/note.service';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-update-note',
-  imports: [],
+  standalone: true,
+  imports: [CommonModule, FormsModule],
   templateUrl: './update-note.html',
-  styleUrl: './update-note.css',
+  styleUrls: ['./update-note.css'],
 })
-export class UpdateNoteComponent {
+export class UpdateNoteComponent implements OnInit {
 
+  id!: number;
+  note: any = {
+    title: '',
+    content: ''
+  };
+
+  constructor(
+    private route: ActivatedRoute,
+    private noteService: NoteService,
+    private router: Router
+  ) {}
+
+  ngOnInit() {
+    this.id = Number(this.route.snapshot.paramMap.get('id'));
+    this.loadNote();
+  }
+
+  loadNote() {
+    this.noteService.getNoteById(this.id).subscribe({
+      next: (data) => {
+        this.note = data;
+      },
+      error: (err) => {
+        console.error("Failed to load note", err);
+      }
+    });
+  }
+
+  updateNote() {
+    this.noteService.updateNote(this.id, this.note).subscribe({
+      next: () => {
+        alert("Note updated!");
+        this.router.navigate(['/notes']);
+      },
+      error: (err) => {
+        console.error("Update failed", err);
+      }
+    });
+  }
 }
